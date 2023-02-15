a81c37e92a3752b72c531efd1b050186b3b236b5
VadimKTS <123896357+VadimKTS@users.noreply.github.com>
2023-02-15 23:42:34 +0300

    Task 1 complete

---
+++ Lesson6HomeWork/Phone.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson6HomeWork
+{
+    internal class Phone
+    {
+        string number;
+        string model;
+        int wheight;
+        //string name;
+
+        public Phone(string number, string model, int wheight)
+        {
+            this.number = number;
+            this.model = model;
+            this.wheight = wheight;
+        }
+
+        public Phone(string number, string model) 
+        {
+            this.number = number;
+            this.model = model;
+        }
+
+        public Phone() { }
+
+
+        public void ReceiveCall(string name)
+        {
+            Console.WriteLine($"Звонит {name}");
+        }
+
+        public string GetNumber()
+        {
+            if (model == null)
+            {
+                Console.WriteLine("Введите номер телефона " + "unknown model");
+            }
+            else
+            {
+                Console.WriteLine("Введите номер телефона " + model);
+            }
+            number = Console.ReadLine();
+            return number;
+        }
+        public void Print()
+        {
+            Console.WriteLine($"Номер: {number}  Модель: {model}  Вес: {wheight}");
+        }
+    }
+}
--- Lesson6HomeWork/Program.cs
+++ Lesson6HomeWork/Program.cs
@@ -1,10 +1,48 @@
-﻿namespace Lesson6HomeWork
+﻿using System.Security.Cryptography;
+using System.Xml.Linq;
+
+namespace Lesson6HomeWork
+
+                                //            1.Класс Phone.
+                                //Создайте класс Phone, который содержит переменные number, model и
+                                //weight.
+                                //Создайте три экземпляра этого класса.
+                                //Выведите на консоль значения их переменных.
+                                //Добавить в класс Phone методы: receiveCall, имеет один параметр – имя
+                                //звонящего.Выводит на консоль сообщение “Звонит { name}”.
+                                //getNumber – возвращает номер телефона.Вызвать эти методы
+                                //для каждого из объектов.
+                                //Добавить конструктор в класс Phone, который принимает на вход три
+                                //параметра для инициализации переменных класса -number, model и weight.
+                                //Добавить конструктор, который принимает на вход два параметра для
+                                //инициализации переменных класса - number, model.
+                                //Добавить конструктор без параметров.
+
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Phone Sony = new Phone( "", "Sony", 324 );
+            Phone Samsung = new ("654656515", "Samsung");
+            Phone Iphone = new ();
+
+            Sony.GetNumber();
+            Samsung.GetNumber();
+            Iphone.GetNumber();
+
+            Sony.Print();
+            Sony.ReceiveCall("Alex");
+
+            Samsung.Print();
+            Samsung.ReceiveCall("Lena");
+
+            Iphone.Print();
+            Iphone.ReceiveCall("Paul");
+
+
+
         }
     }
 }
